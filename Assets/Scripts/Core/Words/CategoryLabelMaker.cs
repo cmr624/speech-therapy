694e5a2aef25144f7ba31a4da9ac1c7a2052d2a9
cmr624 <cmr624@nyu.edu>
2020-09-28 18:11:57 -0700

    adding first content

---
+++ Assets/Scripts/Core/Words/CategoryLabelMaker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Words;
+using Lean.Gui;
+using TMPro;
+using UnityEngine;
+
+namespace Core.Words
+{
+    public class CategoryLabelMaker : MonoBehaviour
+    {
+        [SerializeField] public GameObject LabelPrefab;
+        [SerializeField] public WordLoader Words;
+
+        private int _dictionaryIndex = 0;
+
+        private string[] _keys;
+
+        private string _myKey;
+        void Start()
+        {
+            _keys = Words.Dictionaries[_dictionaryIndex].Keys.ToArray();
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                var newLabel = Instantiate(LabelPrefab, transform);
+                var text = newLabel.GetComponentInChildren<TextMeshProUGUI>();
+                text.text = _keys[i];
+                var btn = newLabel.GetComponent<LeanButton>();
+                btn.OnClick.AddListener(() => OnClick(text));
+            }
+        }
+        
+        
+
+        private void OnClick(TextMeshProUGUI text)
+        {
+            var randomWord = Words.Dictionaries[_dictionaryIndex][text.text][Random.Range(0, Words.Dictionaries[_dictionaryIndex][text.text].Count)];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Words/WordLoader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace Core.Words
+{
+    public class WordLoader : MonoBehaviour
+    {
+        [SerializeField] public List<TextAsset> Assets;
+
+        public List<Dictionary<string, List<string>>> Dictionaries;
+        private void Awake()
+        {
+            
+            Dictionaries = new List<Dictionary<string, List<string>>>();
+            for (int i = 0; i < Assets.Count; i++)
+            {
+                Dictionaries.Add(GenerateDictionaryFromAsset(Assets[i]));
+            }
+
+            var dict = Dictionaries[0];
+
+            
+
+            
+
+        }
+        private Dictionary<string, List<string>> GenerateDictionaryFromAsset(TextAsset csv)
+        {
+            Dictionary<string,List<string>> dictionary = new Dictionary<string, List<string>>();
+            
+            var splitByLine = csv.text.Split('\n');
+            var categories = splitByLine[0].Split(',');
+            
+            // first line is the categories
+            for (int i = 0; i < categories.Length; i++)
+            {
+               dictionary[categories[i]] = new List<string>(); 
+            }
+
+            for (int i = 1; i < splitByLine.Length; i++)
+            {
+                var line = splitByLine[i].Split(',');
+                for (int x = 0; x < line.Length; x++)
+                {
+                    if (line[x].Length > 1)
+                    {
+                       dictionary[categories[x]].Add(line[x]);
+                    } 
+                }
+            }
+
+            return dictionary;
+        }
+    }
+    
+    
+}
